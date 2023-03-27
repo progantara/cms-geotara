bb1f5d5f46fd8c007aa9a12043d24fc9f5432af3
wajisobri <wajisobri@gmail.com>
2023-03-27 18:22:09 +0700

    Added pengelolaan event

--- src/jsx/components/Event/EventList.js
+++ src/jsx/components/Event/EventList.js
@@ -1,90 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Swal from "sweetalert2";
 import DataTable from "react-data-table-component";
+import { deleteEvent, getAllEvents } from "../../../services/EventService";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const EventList = () => {
+	const history = useHistory();
+	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
 	const columns = [
 		{
 			name: "No",
 			selector: (row) => row.no,
 			sortable: true,
-      width: '10%',
+			width: "10%",
 		},
 		{
 			name: "Nama",
 			selector: (row) => row.nama,
 			sortable: true,
-      width: '25%',
+			width: "25%",
 		},
 		{
 			name: "Penyelenggara",
 			selector: (row) => row.penyelenggara,
 			sortable: true,
-      width: '20%',
+			width: "20%",
 		},
 		{
 			name: "Mulai",
 			selector: (row) => row.tanggal_mulai,
 			sortable: true,
-      width: '15%',
-		},
-    {
-			name: "Selesai",
-			selector: (row) => row.tanggal_selesai,
-			sortable: true,
-      width: '15%',
+			width: "15%",
 		},
 		{
 			name: "Aksi",
 			selector: (row) => row.aksi,
-      width: '15%',
-		},
-	];
-
-	const data = [
-		{
-			id: 1,
-			no: "1",
-			nama: "Seren Taun",
-			penyelenggara: "Sobri W",
-			tanggal_mulai: "24 Maret 2023",
-      tanggal_selesai: "26 Maret 2023",
-			aksi: (
-				<div className="d-flex">
-					<Link to="/acara/detail/1" className="btn btn-primary shadow btn-xs sharp me-1">
-						<i className="fas fa-eye"></i>
-					</Link>
-					<Link
-						to="/acara/edit/1"
-						className="btn btn-secondary shadow btn-xs sharp me-1"
-					>
-						<i className="fas fa-pen"></i>
-					</Link>
-					<Link
-						to="#"
-						className="btn btn-danger shadow btn-xs sharp"
-						onClick={() =>
-							swal({
-								title: "Anda yakin ingin menghapus pengguna ini?",
-								text: "Setelah dihapus, Anda tidak akan dapat memulihkannya",
-								icon: "warning",
-								buttons: true,
-								dangerMode: true,
-							}).then((willDelete) => {
-								if (willDelete) {
-									swal("Pengguna telah dihapus!", {
-										icon: "success",
-									});
-								}
-							})
-						}
-					>
-						<i className="fa fa-trash"></i>
-					</Link>
-				</div>
-			),
+			width: "15%",
 		},
 	];
 
@@ -113,7 +68,85 @@ const EventList = () => {
 	};
 
 	// use effect
-	useEffect(() => {}, []);
+	useEffect(() => {
+		setIsLoading(true);
+		getAllEvents()
+			.then((res) => {
+				res.data.data.map((item, index) => {
+					setData((data) => [
+						...data,
+						{
+							no: index + 1,
+							nama: item.nama,
+							penyelenggara: item.organizer,
+							tanggal_mulai: item.start_date,
+							aksi: (
+								<div className="d-flex">
+									<Link
+										// to={`/acara/detail/${item._id}`}
+										className="btn btn-primary shadow btn-xs sharp me-1"
+									>
+										<i className="fas fa-eye"></i>
+									</Link>
+									<Link
+										to={`/acara/edit/${item._id}`}
+										className="btn btn-secondary shadow btn-xs sharp me-1"
+									>
+										<i className="fas fa-pen"></i>
+									</Link>
+									<Link
+										to="#"
+										className="btn btn-danger shadow btn-xs sharp"
+										onClick={() =>
+											Swal.fire({
+												title: "Anda yakin ingin menghapus acara ini?",
+												text: "Setelah dihapus, Anda tidak akan dapat memulihkannya",
+												icon: "warning",
+												showCancelButton: true,
+												confirmButtonColor: "#3085d6",
+												cancelButtonColor: "#d33",
+												confirmButtonText: "Ya, hapus!",
+											}).then((res) => {
+												if (res.isConfirmed) {
+													deleteEvent(item._id)
+														.then((res) => {
+															setData(
+																data.filter(
+																	(element) => element._id !== res.data.data._id
+																)
+															);
+															Swal.fire(
+																"Dihapus!",
+																"Acara telah dihapus.",
+																"success"
+															);
+															history.push("/acara");
+														})
+														.catch((err) => {
+															Swal.fire(
+																"Gagal!",
+																"Acara gagal dihapus",
+																"error"
+															);
+														});
+												}
+											})
+										}
+									>
+										<i className="fa fa-trash"></i>
+									</Link>
+								</div>
+							),
+						},
+					]);
+					setIsLoading(false);
+				});
+			})
+			.catch((err) => {
+				Swal.fire("Gagal!", "Acara gagal dimuat", "error");
+				setIsLoading(false);
+			});
+	}, []);
 
 	return (
 		<div className="col-12">
@@ -138,6 +171,14 @@ const EventList = () => {
 								pagination
 								paginationPerPage={5}
 								customStyles={customStyles}
+								progressPending={isLoading}
+								progressComponent={
+									<ClipLoader
+										color={"#20c997"}
+										loading={isLoading}
+										aria-label="Loading Spinner"
+									/>
+								}
 							/>
 						</div>
 					</div>
